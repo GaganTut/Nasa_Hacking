7e27e4a06bafea15e3701e58143ba66f97d8351d
GaganTut <gstut@live.com>
2017-04-29 12:12:02 -1000

    Modulized map into sep file

---
+++ map.js
@@ -0,0 +1,29 @@
+var locations = [
+      ['Waimanalo Beach', 21.33, -157.68],
+      ['Waikiki Beach', 21.27, -157.82],
+      ['Lanikai Beach', 21.39, -157.71]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(21.43, -158.00),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
