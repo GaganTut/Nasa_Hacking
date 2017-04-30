72067dbb1b934e543dd4deb63e0031d115d357f4
GaganTut <gstut@live.com>
2017-04-30 11:06:29 -1000

    Refactored json parsing

--- public/js/parseJSON.js
+++ public/js/parseJSON.js
@@ -1,8 +1,19 @@
 /*jshint esversion: 6*/
 const parseJSON = (()=> {
 
+let conditions;
+let alerts;
+
+const loadAlerts = (cb) =>{
+  $.getJSON('alerts.json', (json)=> {
+    alerts = json;
+    cb();
+  });
+};
+
 const beachNames = (cb) =>{
   $.getJSON('conditions.json', (json)=> {
+    conditions = json;
     let locations = [];
     for(let k in json) {
       if (json[k].island === 'Oahu') {
@@ -14,17 +25,14 @@ const beachNames = (cb) =>{
 };
 
 const getWeatherInfo = (beachName, cb, element) => {
-  $.getJSON('conditions.json', (json) => {
-    let index = json.map(function(x){return x.beach;}).indexOf(beachName);
-    weather = json[index].weather + ' ' + json[index].temp + '&#8457;';
-    cb(element, weather);
-  });
+  let index = conditions.map(function(x){return x.beach;}).indexOf(beachName);
+  weather = conditions[index].weather + ' ' + conditions[index].temp + '&#8457;';
+  cb(element, weather);
 };
 
 const getWeatherPic = (beachName, cb, element) => {
-  $.getJSON('conditions.json', (json) => {
-    let index = json.map(function(x){return x.beach;}).indexOf(beachName);
-    switch (json[index].weather) {
+    let index = conditions.map(function(x){return x.beach;}).indexOf(beachName);
+    switch (conditions[index].weather) {
       case 'A Few Clouds':
         cb(element, 'public/assets/sunny.jpg');
         break;
@@ -44,13 +52,11 @@ const getWeatherPic = (beachName, cb, element) => {
         cb(element, 'public/assets/sunny.jpg');
         break;
     }
-  });
 };
 
 const getHazInfo = (beachName, cb, element) => {
   $.getJSON('conditions.json', (json) => {
     let index = json.map(function(x){return x.beach;}).indexOf(beachName);
-    console.log(json[index]);
     let info = json[index].description;
     cb(element, info);
   });
