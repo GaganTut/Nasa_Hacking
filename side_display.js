7a6f4fcdb3f5975c95b512b1071b90ff145522ae
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-04-29 14:02:10 -1000

    sidebars 60%

---
+++ side_display.js
@@ -0,0 +1,19 @@
+
+let closeButton = document.querySelector(".closebtn");
+
+closebtn.addEventListener("click", function(){
+  document.getElementById("mySidenav").style.width = "0";
+});
+
+// upon clicking on the markers
+document.getElementById("mySidenav").style.width = "250px";
+
+
+beachName =  document.createElement("h2");
+beachName.innerHTML = ""
+    <h2 id="beachname"> </h2>
+    <a href="">Weather</a>
+    <a href="">Hazards</a>
+    <a href="">Must Do</a>
+
+
