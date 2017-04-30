a07cb636fb159a25b6ee765bcecba40b712e16fa
GaganTut <gstut@live.com>
2017-04-29 19:31:50 -1000

    FIXED BUG ERROR

--- public/js/parseJSON.js
+++ public/js/parseJSON.js
@@ -1,16 +1,15 @@
 
 const parseJSON = (()=> {
 
-const alerts = $.getJSON('alerts.json', (json)=> {
-  return json;
-});
 
-const conditions = $.getJSON('conditions.json', (json)=> {
-  return json;
-});
 
-const beachNames = () =>{
-  console.log(JSON.parse(conditions.responseText));
+const beachNames = (cb) =>{
+  console.log('herreee');
+  console.log($);
+  $.getJSON('conditions.json', (json)=> {
+    console.log(json);
+    cb();
+  });
 };
 
 const getWeatherInfo = (beachName) =>{
